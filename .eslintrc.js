150788035747c2776045e51a661dd1fb67323e0c
joys1006 <joys1006@naver.com>
2021-12-18 18:36:21 +0900

    [FIX]

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,20 @@ module.exports = {
     browser: true,
     node: true
   },
-  extends: ['@vue/standard', 'plugin:vue/essential', 'prettier', '@vue/typescript/recommended'],
+  extends: ['plugin:vue/essential', '@vue/typescript', 'prettier'],
   parserOptions: {
     ecmaVersion: 2020,
     parser: '@typescript-eslint/parser'
   },
-  plugins: ['prettier', '@typescript-eslint'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      'typescript': {}
+    }
+  },
+  plugins: ['@typescript-eslint'],
   rules: {
     semi: ['error', 'always'],
     'space-before-function-paren': [
@@ -22,21 +30,7 @@ module.exports = {
       }
     ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        semi: true,
-        singleQuote: true,
-        useTabs: false,
-        tabWidth: 2,
-        printWidth: 180,
-        bracketSpacing: true,
-        arrowParens: 'always',
-        trailingComma: 'none',
-        vueIndentScriptAndStyle: false
-      }
-    ]
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   overrides: [
     {
